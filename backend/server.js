1c0b015b83dc8cd37e060f8a37bb23641025baa9
unknown <rituprasad2812@gmail.com>
2026-05-31 16:45:48 +0530

    Fix CORS for production

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,10 @@ require('dotenv').config()
 
 const app = express()
 
-app.use(cors())
+app.use(cors({
+  origin: ['https://creamersbakery.netlify.app/', 'http://localhost:5173'],
+  credentials: true
+}))
 app.use(express.json())
 
 mongoose.connect(process.env.MONGO_URI)
